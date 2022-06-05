234617761f855ee295e4e18c98b4473c8df23b84
Alejandro Rodriguez Romero <alex98rodriguezromero@gmail.com>
2022-06-04 21:53:45 -0500

    modify http

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,15 @@ import express from 'express';
 import bodyParse from 'body-parser';
 import { Url } from './common/constants';
 import cors from 'cors';
+import http from 'http';
 import { GetData } from './common/data';
 import { ExtraRoutes } from './common/routes';
 
 const app: express.Application = express();
 const port = Number(process.env.PORT) | 3000;
 
-app.use(bodyParse.json());
 app.use(bodyParse.urlencoded({ extended: true }));
+app.use(bodyParse.json());
 
 app.use(cors({origin: true, credentials: true}));
 
@@ -22,6 +23,8 @@ app.get(Url.get, GetData.get);
 app.use(ExtraRoutes.getExtras);
 app.use(ExtraRoutes.postExtras);
 
-app.listen(port, () => {
+const httpServer = new http.Server(app);
+
+httpServer.listen(port, () => {
   console.log(`🚀 El servidor esta inicializado en el puerto ${port} 🚀`);
 });
